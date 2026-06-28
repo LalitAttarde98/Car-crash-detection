013774170cc6e99e4e7c5613ae5d2122ebc9aa88
LalitAttarde98 <102517237+LalitAttarde98@users.noreply.github.com>
2026-06-28 10:19:13 +0530

    added dinov2_with_windowed_attn.py fix

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -14,21 +14,11 @@ pip install accelerate==1.13.0
 pip install git+https://github.com/huggingface/transformers@v4.49.0-SmolVLM-2
 pip install num2words timm==0.9.0
 
-
-# Fix `dinov2_with_windowed_attn.py`
-FILE_PATH=$(find ~/.local/lib -name "dinov2_with_windowed_attn.py")
-if [ -f "$FILE_PATH" ]; then
-    echo "Found dinov2_with_windowed_attn.py at $FILE_PATH"
-    sed -i.bak '/torch_int,/d' "$FILE_PATH"
-    sed -i '/logger = logging.get_logger(__name__)/a \
+# Fix dinov2_with_windowed_attn.py
+'/logger = logging.get_logger(__name__)/a \
 import torch\
-\
 def torch_int(x):\
     if torch.jit.is_tracing():\
         return x.to(torch.int64)\
     return int(x)\
-' "$FILE_PATH"
-    echo "Patched dinov2_with_windowed_attn.py"
-else
-    echo "Warning: dinov2_with_windowed_attn.py not found. The script might fail."
-fi
+'
